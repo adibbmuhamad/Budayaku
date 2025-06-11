62336ab9bc6e77f58264f7f38e573b42e2db7ef9
Muhamad Adib Minanurohman <inanadib085@gmail.com>
2025-06-11 15:39:43 +0700

    add agenda

---
+++ app/Filament/Resources/AgendaResource.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AgendaResource\Pages;
+use App\Models\Agenda;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class AgendaResource extends Resource
+{
+    protected static ?string $model = Agenda::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-calendar';
+
+    protected static ?string $navigationLabel = 'Agenda';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Textarea::make('description')
+                    ->nullable()
+                    ->maxLength(500),
+                Forms\Components\DateTimePicker::make('start_time')
+                    ->required(),
+                Forms\Components\DateTimePicker::make('end_time')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('start_time')
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('end_time')
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime(),
+            ])
+            ->filters([
+                // Tambahkan filter jika diperlukan
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            // Tambahkan relasi jika diperlukan
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAgendas::route('/'),
+            'create' => Pages\CreateAgenda::route('/create'),
+            'edit' => Pages\EditAgenda::route('/{record}/edit'),
+        ];
+    }
+}
